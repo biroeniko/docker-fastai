a0ed0440dc669fc71c56c9e226a9f67c950a71a6
Biro Eniko <encybiro@gmail.com>
2019-08-25 16:17:58 +0300

    Now working.

--- run-nvidia.sh
+++ run-nvidia.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -v code:/workspace/code -v data:/workspace/data nvcr.io/nvidia/pytorch:19.07-py3
+nvidia-docker run -it -p 8888:8888 --rm -v code:/workspace/code -v data:/workspace/data nvcr.io/nvidia/pytorch:19.07-py3
