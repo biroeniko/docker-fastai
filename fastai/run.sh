7fcef46cfefb5c8cb2d163a47e9605c15a520ee4
Biro Eniko <encybiro@gmail.com>
2019-08-28 22:31:19 +0300

    Now the shared folders are OK.

--- fastai/run.sh
+++ fastai/run.sh
@@ -1,1 +1,3 @@
-nvidia-docker run -it -p 8888:8888 --rm -v code:/workspace/code -v data:/workspace/data fastai
+#!/bin/bash
+DIR=`pwd`
+nvidia-docker run -it -p 8888:8888 --rm -v "${DIR}/code":/workspace/code -v "${DIR}/data":/workspace/data fastai
